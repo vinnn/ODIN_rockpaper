840549dd0e971a957fb3a478833634db971b9808
vinnn <vmoutel@yahoo.fr>
2021-11-25 10:15:23 +0000

    include ui (first working rev)

--- script.js
+++ script.js
@@ -1,40 +1,160 @@
 
 // 
 // 01. Event listener for 'push button'
-//      if button 'data-key' pressed then call playRound
 // 02. Display the Player's selection, the Computer's selection and the result
-// 03. Keep all 5 consecutive results and display score /5
+// 03. Incremenet number of wins up to 5 rounds and display final winner
 
 
-// Initialise the scores
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Initialise the scores and round number
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 let number_of_player_win = 0;
 let number_of_computer_win = 0;
 let number_of_ties = 0;
+let round = 0;
+
+const playerWinsDiv=document.querySelector('.user-score');
+playerWinsDiv.textContent = number_of_player_win;
+const computerWinsDiv=document.querySelector('.computer-score');
+computerWinsDiv.textContent = number_of_computer_win;
+const numberTiesDiv=document.querySelector('.tie-score');
+numberTiesDiv.textContent = number_of_ties;
+const roundDiv=document.querySelector('.round');
+roundDiv.textContent = round;
 
 
-// 01. Press button event listener
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Press button event listener
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 const btns = Array.from(document.querySelectorAll('button'));
 console.log(btns);
 
-// btns.forEach(btn => btn.addEventListener("click", function(e) {
-//     console.log(e);
-//     console.log(e.target.classList[0]);
-// })
-// );
-
-btns.forEach(btn => btn.addEventListener("click", playRound2));
+btns.forEach(btn => btn.addEventListener("click", playRound));
 
 
-
-function playRound2(e) {
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// PlayRound Function
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function playRound(e) {
     
     let computerSelection = computerPlay();
-    console.log("computerSelection :: " + computerSelection.toString());
-
     let playerSelection = e.target.classList[0];
-    console.log("playerSelection :: " + playerSelection);
+    let roundResult = whoWinsRound(playerSelection, computerSelection);
 
-    let roundResult;
+    displayRound(computerSelection, playerSelection, roundResult);
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// PlayRound Function
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function displayRound(computerSelection, playerSelection, roundResult) {
+
+
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // read previous round number
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    const roundDiv=document.querySelector('.round');
+    round = Number(roundDiv.textContent);
+
+
+
+
+
+    // If previous_round number is 5 then
+    //      new round = 1
+    //      nb player win = nb computer win = number ties = 0
+    // else
+    //      round += 1
+    //      nb wins += 1
+
+    if (round == 5) {
+            // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            // intitalise to 0. Will be incremented in the common calcs after
+            // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+            round = 0;
+            number_of_player_win = 0;
+            number_of_computer_win = 0;
+            number_of_ties = 0;
+    } else {
+            // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            // record previous scores
+            // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            const playerWinsDiv=document.querySelector('.user-score');
+            number_of_player_win = Number(playerWinsDiv.textContent);
+            const computerWinsDiv=document.querySelector('.computer-score');
+            number_of_computer_win = Number(computerWinsDiv.textContent);
+            const numberTiesDiv=document.querySelector('.tie-score');
+            number_of_ties = Number(numberTiesDiv.textContent);
+    }
+
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    //  Increment the round number and the number of wins or ties:  
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%      
+    if (roundResult == "You win!") {
+        number_of_player_win += 1;
+    } else if (roundResult == "You lose!") {
+        number_of_computer_win += 1;
+    } else if (roundResult == "Tie!") {
+        number_of_ties += 1;        
+    }
+    round += 1;
+
+
+
+
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // DISPLAY UPDATED RESULTS
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+    // Display the round results
+    const playerDiv=document.querySelector('.ctn-user-choice');
+    playerDiv.textContent = playerSelection;
+    const computerDiv=document.querySelector('.ctn-computer-choice');
+    computerDiv.textContent = computerSelection;
+    const resultDiv=document.querySelector('.result');
+    resultDiv.textContent = roundResult;
+
+    // Write new scores
+    playerWinsDiv.textContent = number_of_player_win;
+    computerWinsDiv.textContent = number_of_computer_win;
+    numberTiesDiv.textContent = number_of_ties;
+    roundDiv.textContent = round;
+
+    // Final result (displayed at end of round 5 only)
+    let final_message = (number_of_player_win == number_of_computer_win) ? "Game Tie..." : (number_of_player_win > number_of_computer_win) ? "The final winner is the Player!" : "The final winner is the Computer!";
+    // let final_message = "The final winner is the " + final_winner;
+
+    const finalResultDiv=document.querySelector('.final-result');
+    if (round == 5) {
+        finalResultDiv.textContent = final_message;
+    } else {
+        finalResultDiv.textContent = "";
+    }
+
+}
+
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Computer selection (random)
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function computerPlay() {
+    let random_number = Math.floor(Math.random() * 2.99);
+
+    if (random_number == 0) {
+        return "rock";
+    } else if (random_number == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// Determines who wins or if a tie
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function whoWinsRound(playerSelection, computerSelection) {
 
     if (playerSelection == computerSelection) {
         roundResult = "Tie!";
@@ -51,177 +171,5 @@ function playRound2(e) {
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         roundResult = "You win!";
     }
-
-    console.log("roundResult :: " + roundResult);
-
-    displayRound(computerSelection, playerSelection, roundResult);
-
-    // return roundResult;
-
-
+    return roundResult;
 }
-
-
-function displayRound(computerSelection, playerSelection, roundResult) {
-
-
-    // Display the round results
-    const playerDiv=document.querySelector('.ctn-user-choice');
-    playerDiv.textContent = playerSelection;
-
-    const computerDiv=document.querySelector('.ctn-computer-choice');
-    computerDiv.textContent = computerSelection;
-
-    const resultDiv=document.querySelector('.result');
-    resultDiv.textContent = roundResult;
-
-
-    // Read previous scores and round number
-    const playerWinsDiv=document.querySelector('.user-score');
-    number_of_player_win = Number(playerWinsDiv.textContent);
-    const computerWinsDiv=document.querySelector('.computer-score');
-    number_of_computer_win = Number(computerWinsDiv.textContent);
-    const numberTiesDiv=document.querySelector('.tie-score');
-    number_of_ties = Number(numberTiesDiv.textContent);
-    const roundDiv=document.querySelector('.round');
-    round = Number(roundDiv.textContent);
-
-    //  Increment the number of wins or ties:    
-    if (roundResult == "You win!") {
-        number_of_player_win += 1;
-    } else if (roundResult == "You lose!") {
-        number_of_computer_win += 1;
-    } else if (roundResult == "Tie!") {
-        number_of_ties += 1;        
-    }
-
-    round += 1;
-    // if (round == 6) {
-
-    // }
-
-
-    // Write new scores
-    playerWinsDiv.textContent = number_of_player_win;
-    computerWinsDiv.textContent = number_of_computer_win;
-    numberTiesDiv.textContent = number_of_ties;
-    roundDiv.textContent = round;
-
-
-
-
-
-    // Read the current round number
-
-
-    console.log("round :: " + round);
-
-
-
-
-}
-
-
-
-
-
-function displayGame(round) {
-
-    const playerDiv=document.querySelector('.ctn-user-choice');
-    playerDiv.textContent = playerSelection;
-
-    const computerDiv=document.querySelector('.ctn-computer-choice');
-    computerDiv.textContent = computerSelection;
-
-    const resultDiv=document.querySelector('.result');
-    resultDiv.textContent = resultRound;
-
-    const gameResultDiv=document.querySelector('.game-results');
-    gameResultDiv.textContent = resultRound;
-
-
-
-
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Computer selection (random)
-function computerPlay() {
-    let random_number = Math.floor(Math.random() * 2.99);
-
-    if (random_number == 0) {
-        return "rock";
-    } else if (random_number == 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
-
-
-
-
-
-
-
-
-
-
-// Game of 5 rounds 
-// if Player selection is "none" (invalid user entry) then re-call PlayerPlay 
-// Increment record of player and computer wins for calcualtion of final winner
-// let number_of_player_win = 0;
-// let number_of_computer_win = 0;
-
-// function game() {
-
-//     for (let i = 1; i < 6; i++) {
-
-//         let playerSelection = "none";
-//         while (playerSelection == "none") {
-//             playerSelection = playerPlay();
-//         }
-
-//         let computerSelection = computerPlay();
-//         let roundResult = playRound(playerSelection,computerSelection);
-
-//         if (roundResult == "You win!") {
-//             number_of_player_win += 1;
-//         } else if (roundResult == "You lose!") {
-//             number_of_computer_win += 1;
-//         }
-
-//         console.log("Round No." + i + " - You: " + playerSelection + " vs Computer: " + computerSelection + "      => result " + roundResult);
-//     }
-
-//     let final_winner = (number_of_player_win == number_of_computer_win) ? "Neither the Player nor the Computer..." : (number_of_player_win > number_of_computer_win) ? "Player!" : "Computer!";
-    
-//     console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-//     console.log("The final winner is the " + final_winner);
-//     console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-
-// }
-
-// Call to run the game
-// game()
-
-
